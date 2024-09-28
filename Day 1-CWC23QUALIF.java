930b649eab01f54cab09f485bf138fd90d6fce9a
yuvraj860 <yuvrajrathore860@gmail.com>
2024-09-28 23:59:36 +0530

    Add files via upload

---
+++ Day 1-CWC23QUALIF.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int x;
+	Scanner in=new Scanner(System.in);
+	x=in.nextInt();
+	if(x>=12){
+	    System.out.println("Yes");
+	}
+	else{
+	      System.out.println("no");
+	}
+
+	}
+}
