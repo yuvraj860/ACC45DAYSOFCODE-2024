8411a489185a97c73f144dda19006d8e8ee5186b
yuvraj860 <yuvrajrathore860@gmail.com>
2024-10-13 21:56:41 +0530

    Add files via upload

---
+++ Day9_LCMOfNumbers.java
@@ -0,0 +1,23 @@
+package Simple.com;
+
+public class Day9_LCMOfNumbers {
+
+        public static void main(String[] args)
+        {
+
+            int a = 15, b = 25;
+
+            int ans = (a > b) ? a : b;
+
+            while (true) {
+                if (ans % a == 0 && ans % b == 0)
+                    break;
+                ans++;
+            }
+
+            System.out.println("LCM of " + a + " and " + b
+                    + " : " + ans);
+        }
+    }
+
+
