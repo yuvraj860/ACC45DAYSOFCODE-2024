5c24efd60d42e887efe900897848554a5e17808f
yuvraj860 <yuvrajrathore860@gmail.com>
2024-10-13 21:50:15 +0530

    Add files via upload

---
+++ Day7_NeonNumber.java
@@ -0,0 +1,54 @@
+package Simple.com;
+import java.io.*;
+public class Day7_NeonNumber {
+
+        public static boolean checkNeon(int n)
+        {
+
+            int square = n * n;
+
+
+            int sum = 0;
+
+
+            while (square > 0) {
+
+
+                int r = square % 10;
+
+
+                sum += r;
+
+
+                square = square / 10;
+            }
+
+
+            if (sum == n)
+
+
+                return true;
+            else
+
+
+                return false;
+        }
+
+
+        public static void main(String[] args)
+        {
+
+             int n = 9;
+
+            if (checkNeon(n))
+
+                System.out.println("Given number " + n
+                        + " is Neon number");
+            else
+
+                System.out.println("Given number " + n
+                        + " is not a Neon number");
+        }
+    }
+
+
