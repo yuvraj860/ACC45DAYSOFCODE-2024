7ab05bf0b8f2fa3023ed5190c6bf0751a2376df6
yuvraj860 <yuvrajrathore860@gmail.com>
2024-10-13 21:44:19 +0530

    Add files via upload

---
+++ Day6_FactorsOfPositiveinteger.java
@@ -0,0 +1,24 @@
+package Simple.com;
+
+public class Day6_FactorsOfPositiveinteger {
+
+
+        public static void main(String[] args) {
+
+            // positive number
+            int number = 60;
+
+            System.out.print("Factors of " + number + " are: ");
+
+            // loop runs from 1 to 60
+            for (int i = 1; i <= number; ++i) {
+
+                // if number is divided by i
+                // i is the factor
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
